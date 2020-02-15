0d0650475936f6f48758091ecd2402200e17e9db
Dean Roskell <roskelld@gmail.com>
2020-02-14 19:59:55 -0700

    First commit
    Setup basic locomotion system with test level
    Walking, jumping, crouch and first person camera configured.

--- Source/FantasyRaces/Private/BaseCharacter.cpp
+++ Source/FantasyRaces/Private/BaseCharacter.cpp
@@ -2,12 +2,29 @@
 
 
 #include "BaseCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/PawnMovementComponent.h"
 
 // Sets default values
 ABaseCharacter::ABaseCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	CameraAttachSocketName = TEXT("head");
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->bUsePawnControlRotation = true;	
+
+	GetMovementComponent()->GetNavAgentPropertiesRef().bCanCrouch = true;
+	GetMovementComponent()->GetNavAgentPropertiesRef().bCanFly = false; 
+	GetMovementComponent()->GetNavAgentPropertiesRef().bCanJump = true;
+	
+	// FOV settings
+	DefaultFOV = 90.0f;
+	FocusFOV = 120.0f;
+	FocusInterpSpeed = 20.f;
+
 
 }
 
@@ -16,6 +33,66 @@ void ABaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// Update FOV
+	CameraComp->SetFieldOfView(DefaultFOV);
+
+	// Only run on the server
+	//if (GetLocalRole() == ROLE_Authority)
+	//{
+
+	//}
+
+	// Attach the Camera to the Mesh Head
+	CameraComp->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, CameraAttachSocketName);
+
+	// Set Camera offset to match face location
+	CameraComp->SetRelativeLocation(CameraOffset);
+}
+
+// Character Interactions 
+
+void ABaseCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector() * Value);
+}
+
+
+
+void ABaseCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector() * Value);
+}
+
+void ABaseCharacter::StartFocus()
+{
+	bRequestedFocus = true;
+}
+
+void ABaseCharacter::EndFocus()
+{
+	bRequestedFocus = false;
+}
+
+void ABaseCharacter::StartCrouch()
+{
+	Crouch();
+}
+
+void ABaseCharacter::EndCrouch()
+{
+	UnCrouch();
+}
+
+void ABaseCharacter::ToggleCrouch()
+{
+	if (bIsCrouched)
+	{
+		UnCrouch();
+	}
+	else
+	{
+		Crouch();
+	}
 }
 
 // Called every frame
@@ -23,12 +100,36 @@ void ABaseCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	float TargetFOV = bRequestedFocus ? FocusFOV : DefaultFOV;
+
+	float NewFOV = FMath::FInterpTo(CameraComp->FieldOfView, TargetFOV, DeltaTime, FocusInterpSpeed);
+
+	CameraComp->SetFieldOfView(NewFOV);
+
 }
 
 // Called to bind functionality to input
 void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Movement 
+	PlayerInputComponent->BindAxis("MoveForward", this, &ABaseCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ABaseCharacter::MoveRight);
+
+	// Rotation
+	PlayerInputComponent->BindAxis("CameraPitch", this, &ABaseCharacter::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("CameraYaw", this, &ABaseCharacter::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction("Focus", IE_Pressed, this, &ABaseCharacter::StartFocus);
+	PlayerInputComponent->BindAction("Focus", IE_Released, this, &ABaseCharacter::EndFocus);
+
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ABaseCharacter::StartCrouch);
+	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ABaseCharacter::EndCrouch);
+	PlayerInputComponent->BindAction("CrouchToggle", IE_Pressed, this, &ABaseCharacter::ToggleCrouch);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ABaseCharacter::Jump);
+	
 
 }
 
--- Source/FantasyRaces/Public/BaseCharacter.h
+++ Source/FantasyRaces/Public/BaseCharacter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "BaseCharacter.generated.h"
 
+class UCameraComponent;
+
 UCLASS()
 class FANTASYRACES_API ABaseCharacter : public ACharacter
 {
@@ -18,6 +20,48 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UCameraComponent* CameraComp;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Player")
+	FName CameraAttachSocketName;
+
+	// Movement Functions
+
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	void StartFocus();
+
+	void EndFocus();
+
+	void StartCrouch();
+
+	void EndCrouch();
+
+	void ToggleCrouch();
+
+
+
+
+	bool bRequestedFocus; 
+
+	/* Character's default FOV */
+	UPROPERTY(EditDefaultsOnly, Category = "Player", meta = (ClampMin = 10, ClampMax = 150))
+	float DefaultFOV;
+
+	/* FOV used for focus vision */
+	UPROPERTY(EditDefaultsOnly, Category = "Player", meta = (ClampMin = 10, ClampMax = 150))
+	float FocusFOV;
+
+	/* Speed to reach maximum focus */
+	UPROPERTY(EditDefaultsOnly, Category = "Player", meta = (ClampMin = 0.1, ClampMax = 100))
+	float FocusInterpSpeed;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Player")
+	FVector CameraOffset;
 
 public:	
 	// Called every frame
